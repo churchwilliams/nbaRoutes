c47adb7df01f2d1ec2490bd02ceccc2aa3769813
Eric Williams <eric@churchwilliams.com>
2015-02-25 12:18:40 -0700

    Pretty much done. REALLY difficult though. Need to do again.

--- js/app.js
+++ js/app.js
@@ -4,4 +4,26 @@ app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
   //router here
+  $routeProvider
+	.when('/', {
+		templateUrl: 'js/home/homeTmpl.html',
+		controller: 'homeCtrl',
+		resolve: {
+			allData: function($route, homeService) {
+				return homeService.getAllData();
+			}
+		}
+	})
+	.when('/teams/:team', {
+		templateUrl: 'js/teams/teamTmpl.html',
+		controller: 'teamCtrl',
+		resolve: {
+			teamData: function($route, teamService){
+				return teamService.getTeamData($route.current.params.team); // This is will call the getTeamData method from the teamService.js. This will return a promise, once resolved, it will give us the data here in the controller.
+			}
+		}
+	})
+	.otherwise ({
+		redirectTo: '/' // redirects to the index page if the above does not work
+	});
 });
